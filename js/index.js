43e890980aac253cd416f6fc4e8b03a11fd9c9ae
Tzu-Yin Hong <lili668668@gmail.com>
2017-09-08 11:16:22 +0800

    add schedule

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var text_list = [
+	"進來的人考試都可以考一百分",
+	"空白的背景也是很美的",
+	"空白不是因為我不會設計背景喔",
+	"襪owo~~，你發現彩蛋了"
+];
+
+$(function() {
+	var surprise_click = function() {
+		$("#change_text").text(random_text());
+	};
+	
+	var random_text = function() {
+		var rand_num = Math.floor(Math.random() * text_list.length);
+		return text_list[rand_num];
+	};
+	
+	$("#surprise").click(surprise_click);
+});
